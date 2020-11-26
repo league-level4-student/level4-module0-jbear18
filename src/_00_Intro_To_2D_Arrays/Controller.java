95aba3cc8b41eae93f48334cd9daedbe62efe592
jbear18 <shehu01@hotmail.com>
2020-11-25 20:01:09 -0800

    looking-around

--- src/_00_Intro_To_2D_Arrays/Controller.java
+++
@@ -1,6 +0,0 @@
-package _00_Intro_To_2D_Arrays;
-
-public interface Controller{
-public int setSpeed();
-public int getSpeed();
-}
---
+++ src/_00_Intro_To_2D_Arrays/SnakeGame.java
@@ -0,0 +1,10 @@
+package _00_Intro_To_2D_Arrays;
+
+public class SnakeGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+new GameFrame();
+	}
+
+}
